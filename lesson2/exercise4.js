6f64350e4e0e5608da77d2053d4be1b57b870966
Denis <fomk@mail.ru>
2021-05-11 00:01:27 +0300

    lesson2

---
+++ lesson2/exercise4.js
@@ -0,0 +1,36 @@
+// Присвоить переменной а значение в промежутке [0..15]. С помощью оператора switch организовать вывод чисел от a до 15.
+var a = 12;
+
+switch (a) {
+    case 1:
+        alert(a); a++;
+    case 2:
+        alert(a); a++;
+    case 3:
+        alert(a); a++;
+    case 4:
+        alert(a); a++;
+    case 5:
+        alert(a); a++;
+    case 6:
+        alert(a); a++;
+    case 7:
+        alert(a); a++;
+    case 8:
+        alert(a); a++;
+    case 9:
+        alert(a); a++;
+    case 10:
+        alert(a); a++;
+    case 11:
+        alert(a); a++;
+    case 12:
+        alert(a); a++;
+    case 13:
+        alert(a); a++;
+    case 14:
+        alert(a); a++;
+    case 15:
+        alert(a); a++;
+
+}
